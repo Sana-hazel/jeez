19c24be204b88b268590b72ee08ae6c27aaa5e58
Sana-hazel <sanathoisinghnongthombam@gmail.com>
2026-02-12 00:41:07 +0530

    second commit

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -2,6 +2,6 @@ package demo;
 public class App
 {
     public static void main(String[] args) {
-        System.out.println("Hello,world");
+        System.out.println("Hello,world dizzy");
     }
 }
